1854bee045f4a16648a8fc554b44903c748e767e
Longkuroko <longsteve.jobs@gmail.com>
2022-08-18 22:07:51 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contact } from  './components/Contact';
+import { Footer } from './components/Footer';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,6 +14,10 @@ function App() {
     <div className="App">
         <NavBar />
         <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,56 @@
+import { useState, useEffect } from 'react'; 
 import { Container, Row, Col } from "react-bootstrap"
 import React from "react";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.sgv"
+import headerImg from "../assets/img/header-img.svg"
 
 export const Banner = () => {
+
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Software Engineer, Data Engineer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+        
+        return () => { clearInterval(ticker);}
+    },[text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updateText = isDeleting 
+            ? fullText.substring(0, text.length - 1)
+            : fullText.substring(0, text.length + 1);
+        
+        setText(updateText);
+        
+        if(isDeleting) {
+            setDelta(prevDelta => prevDelta /2);
+        }
+
+        if (!isDeleting && updateText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updateText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Long - `}<span className="wrap">Software Engineer</span></h1>
+                        <h1>{`Hi I'm Long - `}<span className="wrap">{text}</span></h1>
                         <p>Lorem ipsum dolor sit am Lorem ipsum dolor sit am Lorem ipsum dolor sit am Lorem ipsum dolor sit am</p>
                         <button onClick={() => console.log('connect')}>Let's connect</button>
                     </Col>
